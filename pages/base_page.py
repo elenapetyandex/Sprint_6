9b6db9eb07d101f7fbf77b56b658fcadfa81d79f
Elena Pet <dragonqueen3@yandex.ru>
2025-03-14 21:55:51 +0700

    change call to def(find_element) in base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
+from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-from urls import Urls
+
 
 
 class BasePage:
@@ -9,10 +10,11 @@ class BasePage:
         self.driver = driver
 
     def find_element(self, locator):
-        self.driver.find_element(*locator)
+        return self.driver.find_element(*locator)
 
     def click_on_element(self, locator):
-        self.driver.find_element(*locator).click()
+        element = self.find_element(locator)
+        element.click()
 
     def wait_visibility_of_element(self, locator):
         return WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(locator))
@@ -26,7 +28,7 @@ class BasePage:
         return WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(locator))
 
     def scroll_to_element(self, locator):
-        element = self.driver.find_element(*locator)
+        element = self.find_element(locator)
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
 
@@ -38,10 +40,11 @@ class BasePage:
 
 
     def fill_input_field(self, locator, input):
-        self.driver.find_element(*locator).send_keys(input)
+        self.find_element(locator).send_keys(input)
 
     def clear_field(self, locator):
-        self.driver.find_element(*locator).clear()
+        self.find_element(locator).clear()
+
 
     def get_current_url (self):
         current_url = self.driver.current_url
@@ -49,28 +52,32 @@ class BasePage:
 
 
     def find_elements(self, locator):
-        self.driver.find_elements(*locator)
+        return self.driver.find_elements(*locator)
 
     def get_text(self, locator):
-        element = self.driver.find_element(*locator)
+        element = self.find_element(locator)
         return element.text
 
     def get_value(self, locator):
-        element = self.driver.find_element(*locator)
+        element = self.find_element(locator)
         return element.get_attribute('value')
 
     def choose_element_by_count(self, locator, count):
-        elements = self.driver.find_elements(*locator)
+        elements = self.find_elements(locator)
         element = elements[count-1]
         element.click()
 
 
 
-    def wait_url(self, Url):
-        return WebDriverWait(self.driver, 5).until(expected_conditions.url_contains(Url))
+    def wait_url(self, url):
+        return WebDriverWait(self.driver, 5).until(expected_conditions.url_contains(url))
 
     def switch_window(self):
         self.driver.switch_to.window(self.driver.window_handles[1])
+
+    def get_url(self, url):
+        self.driver.get(url)
+
 
 
 
