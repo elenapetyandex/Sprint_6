8e4ffed172d2259a9b7f129b40399489dd817074
Elena Pet <dragonqueen3@yandex.ru>
2025-03-14 21:53:23 +0700

    notice1: remove second fixture and add def(get_url) in tests

--- conftest.py
+++ conftest.py
@@ -7,17 +7,13 @@ from urls import Urls
 @pytest.fixture
 def driver():
     driver = webdriver.Firefox()
-    driver.get(Urls.SCOOTER_ORDER_URL)
     yield driver
     driver.quit()
 
 
 
-@pytest.fixture
-def driver_1(driver):
-    driver.get(Urls.SCOOTER_HOMEPAGE_URL)
-    yield driver
-    driver.quit()
+
+
 
 
 
