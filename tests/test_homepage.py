821f235f442f7f81877adc0bc9981ba1ac248257
Elena Pet <dragonqueen3@yandex.ru>
2025-03-15 23:23:37 +0700

    add allure titles for tests

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -9,9 +9,7 @@ import allure
 
 class TestHomepage:
 
-
-
-
+    @allure.title('Проверка видимости текста ответа при клике на вопрос в блоке "Вопросы о важном"')
     @pytest.mark.parametrize('question_locator,answer_text', Data.question_answer_list)
     def test_get_answer_for_question_0(self, driver, question_locator, answer_text):
         driver.get(Urls.SCOOTER_HOMEPAGE_URL)
@@ -20,8 +18,7 @@ class TestHomepage:
         homepage.select_question_from_list(question_locator)
         assert homepage.check_visibility_answer(answer_text)
 
-
-
+    @allure.title('Проверка перехода на страницу заказа при клике на кнопку "Заказать" в верхнем правом углу страницы')
     def test_upper_order_button(self, driver):
         driver.get(Urls.SCOOTER_HOMEPAGE_URL)
         homepage = Homepage(driver)
@@ -30,6 +27,7 @@ class TestHomepage:
         homepage.get_current_url()
         assert homepage.get_current_url() == Urls.SCOOTER_ORDER_URL
 
+    @allure.title('Проверка перехода на страницу заказа при клике на кнопку "Заказать" внизу страницы')
     def test_lower_order_button(self, driver):
         driver.get(Urls.SCOOTER_HOMEPAGE_URL)
         homepage = Homepage(driver)
@@ -38,6 +36,7 @@ class TestHomepage:
         homepage.lower_order_button_click()
         assert homepage.get_current_url() == Urls.SCOOTER_ORDER_URL
 
+    @allure.title('Проверка перехода на страницу "ЯндексДзен" при клике на логотипа "Яндекс" в верхнем левом углу страницы')
     def test_yandex_logo_go_to_dzen(self, driver):
         driver.get(Urls.SCOOTER_HOMEPAGE_URL)
         homepage = Homepage(driver)
@@ -47,7 +46,7 @@ class TestHomepage:
         homepage.wait_url(Urls.DZEN_URL)
         assert  Urls.DZEN_URL in homepage.get_current_url()
 
-
+п
 
 
 
--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,4 +1,6 @@
+import allure
 
+from helpers import Helpers
 from urls import Urls
 from pages.order_page import OrderPage
 
@@ -7,38 +9,49 @@ from data import Data
 
 
 
-class TestOrderPage():
-
-    def tests_select_station_set_value_in_field(self, driver,station='Пушкинская'):
+class TestOrderPage:
+    @allure.title('Выбор станции метро из списка задает значение погля равным выбранной станции')
+    @pytest.mark.parametrize('station', Data.metro_station)
+    def tests_select_station_set_value_in_field(self, driver, station):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
         orderpage.set_station_by_select(station)
         assert orderpage.check_metro_station_set_up() == station
 
-    def tests_input_station_by_typing_calls_station_to_select(self, driver,station='Пушкинская'):
+    @allure.title('При печатании существующей станции метро в поле, можно  соотвествующая станция из списка кликабельна')
+    @pytest.mark.parametrize('station', Data.metro_station)
+    def tests_input_station_by_typing_calls_station_to_select(self, driver, station):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
         assert orderpage.select_metro_station_by_typing(station)
 
+    @allure.title('Заполнение формы валидными данными и нажатие кнопки "Далее" переводит на форму "Про аренду"')
     @pytest.mark.parametrize('name,surname,address, phone, station', Data.data_form_person)
     def test_filled_form_person_about(self, driver,  name, surname,address, phone, station):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
         orderpage.fill_form_about_person( name, surname, address, phone, station)
         orderpage.click_button_next()
         assert orderpage.wait_visible_form_about_rent()
 
+    @allure.title('Нажатие кнопки "Назад" под формой "Про аренду" ведет к форме "Для кого самокат"')
     @pytest.mark.parametrize('name,surname,address, phone, station', Data.data_form_person_about_1)
     def test_click_back_button_on_about_rent_form_go_to_person_about_form(self, driver, name, surname, address, phone, station):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
         orderpage.fill_form_about_person(name, surname, address, phone, station)
         orderpage.click_button_next()
         assert orderpage.click_back_button()
 
-    @pytest.mark.parametrize('name,surname,address, phone, station', Data.data_form_person_about_2)
+    @allure.title('Выбор даты кликом в календаре в поле "Когда привезти самокат" устанавливает значение поля равным выбранной дате')
+    @pytest.mark.parametrize('name,surname,address, phone,station', Data.data_form_person_about_2)
     @pytest.mark.parametrize('month,day', Data.date_to_get_scooter)
     def test_fill_input_when_get_scooter_by_select(self, driver, name, surname, address, phone, station, month, day):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
         orderpage.fill_form_about_person(name, surname, address, phone, station)
@@ -46,40 +59,38 @@ class TestOrderPage():
         orderpage.set_month(month, day)
         assert day in orderpage.get_setted_date() and month in orderpage.get_setted_date()
 
-    def test_fill_input_when_get_scooter_by_typing(self,driver, time_get_scooter):
-        orderpage = OrderPage(driver)
-
-
-
-
-
-    def test_choose_rent_period(self, driver, rent_period=3):
-
+    @allure.title('Проверка установления значения срока аренды в форме заказа соответствует выбранному сроку из списка кликом')
+    @pytest.mark.parametrize('name,surname,address, phone,station', Data.data_form_person_about_1)
+    @pytest.mark.parametrize('rent_period', Data.rent_period)
+    def test_choose_rent_period(self, driver, rent_period, name, surname, address, phone, station):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
-        orderpage.fill_form_about_person('анастасия', 'ивановна', 'Маяковского', '89999999999', 'Пушкинская')
+        orderpage.fill_form_about_person(name, surname, address, phone, station)
         orderpage.click_button_next()
         orderpage.set_rent_period(rent_period)
         assert 'трое суток' in  orderpage.all_form_value()
 
-
-
+    @allure.title('Проверка значение цвета самоката в форме заказа соответсвует выбору чекбокса')
     @pytest.mark.parametrize('color', Data.data_color)
-    def test_chekbox_choose_color(self, driver, color):
+    @pytest.mark.parametrize('name,surname,address, phone, station', Data.data_form_person_about_1)
+    def test_chekbox_choose_color(self, driver, color, name, surname, address, phone, station):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
-        orderpage.fill_form_about_person('анастасия', 'ивановна', 'Маяковского', '89999999999', 'Пушкинская')
-        orderpage.click_metro_station()
-        orderpage.select_metro_station_from_list()
+        orderpage.fill_form_about_person(name, surname, address, phone, station)
         orderpage.click_button_next()
         orderpage.select_scooter_color(color)
         assert color in  orderpage.all_form_value()
 
+    @allure.title('Проверка появления формы "Заказ оформлен" при заполнении форм заказа и нажатии на кнопку "Да" в окне "Хотите оформить заказ?"')
     @pytest.mark.parametrize('date, rent_period, color,comment', Data.data_form_about_rent)
-    def test_get_order(self, driver,date, rent_period, color, comment):
+    @pytest.mark.parametrize('name,surname,address,phone,station', Data.data_form_person_about_1)
+    def test_get_order(self, driver,date, rent_period, color, comment, name, surname, address, phone, station):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
-        orderpage.fill_form_about_person('анастасия', 'ивапн', 'лаоарппа', '89999999999', 'Пушкинская')
+        orderpage.fill_form_about_person(name, surname, address, phone, station)
         orderpage.click_button_next()
         orderpage.fill_input_when_get_scooter_by_typing(date)
         orderpage.set_rent_period(rent_period)
@@ -89,7 +100,9 @@ class TestOrderPage():
         orderpage.click_yes_button()
         assert orderpage.wait_order_done_pop_up()
 
+    @allure.title('Проверка перехода на главную страницу при нажатии на логотип "Скутер"')
     def test_click_scooter_logo_go_to_homepage(self, driver):
+        driver.get(Urls.SCOOTER_ORDER_URL)
         orderpage = OrderPage(driver)
         orderpage.wait_visible_orderpage()
         orderpage.click_logo_scooter()
